bbc7886afb1792482ee62fca08adee9938c99c10
chengan <1825572930@qq.com>
2018-10-17 20:04:23 +0800

    新添加内容
    newworld

---
+++ HelloWorld/newworld/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+#coding:utf-8
+from django.http import HttpResponse
+def index(request):
+       #return HttpResponse(u"Hello World! 大家好！")
+       return render(request,"index.html",)
